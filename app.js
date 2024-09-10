dca512a435ee6e27b6e35b810bc476aa77ab58cc
ScottSyverson <166549266+ScottSyverson@users.noreply.github.com>
2024-09-10 15:35:28 -0500

    up and running correctly and styled

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
-let newTask = document.getElementById("newTaskText");
 
-let submitButton = document.getElementById("submitButton");
-
-submitButton.addEventListener("click", addTaskToList)
-
-
-
-
-function addTaskToList() {
-    let ul = document.getElementById("listedTasks");
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(newTask.value));
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "\u00D7";
-    deleteButton.style.borderRadius = "50%";
-    li.appendChild(deleteButton);
-    ul.appendChild(li);
-    deleteButton.addEventListener("click", deleteTask)
-}
-
-function deleteTask() {
-
-    
-
-
-    console.log("WHAT")
-    
-
+let myFullList = document.getElementsByTagName("LI")
+let i;
+for (i = 0; i < myFullList.length; i++) {
+  let thisButton = document.createElement("button");
+  let xButton = document.createTextNode("delete");
+  thisButton.className = "close";
+  thisButton.appendChild(xButton);
+  myFullList[i].appendChild(thisButton);
 
 }
 
-
-
-
-/*
-
-
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
+let close = document.getElementsByClassName("close");
+let c;
+for (c = 0; c < close.length; c++) {
+  close[c].onclick = function () {
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
+let list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
 
-*/
+function newElement(){
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("taskInput").value;
+  let textTask = document.createTextNode(inputValue);
+  li.appendChild(textTask);
+
+  if(inputValue === ''){
+    alert("Please enter a task.");
+  } else { 
+    document.getElementById("theList").appendChild(li);
+  }
+  
+  document.getElementById("taskInput").value = "";
+  let thisButton = document.createElement("button");
+  let txt = document.createTextNode("delete");
+  thisButton.className = "close"
+  thisButton.appendChild(txt);
+  li.appendChild(thisButton);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+}
+
+thisButton.html = "green"
